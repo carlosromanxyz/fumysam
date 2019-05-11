2ae441b34bb68816ec1e6fb3695fdebdfaf6dc3b
carlosromanxyz <carlosromanxyz@iMac-de-Carlos.local>
2019-05-10 20:57:35 -0400

    Remove capability for access to ACF Admin for all users

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -61,3 +61,19 @@ function wpdocs_custom_excerpt_length( $length ) {
     return 8;
 }
 add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
+
+/**
+ * ACF Allow admin for certain users
+ */
+add_filter('acf/settings/show_admin', 'my_acf_show_admin');
+function my_acf_show_admin($show) {
+	// provide a list of usernames who can edit custom field definitions here
+	$admins = array( 
+		'carlosromanxyz'
+	);
+
+	// get the current user
+	$current_user = wp_get_current_user();
+
+	return (in_array($current_user->user_login, $admins));
+}
